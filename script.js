7c950628ee1131ede16c200f97390ccb04758e4b
Chris MacGeorge <christopher.s.macgeorge@gmail.com>
2020-09-21 17:57:53 -0700

    updated js five day forecast

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ $("#searchBtn").on("click", function(event) {
     var cityName = $("#cityName").val();
     console.log(cityName); 
     forecast(cityName);
+    fiveDayForecast(cityName)
 })
 
 function forecast (cityName){
@@ -14,5 +15,38 @@ function forecast (cityName){
         method: "GET"
     }).then (function(apiData){
         console.log(apiData);
+        $("#currentData").html(`
+        <div class='card'>
+        <h6>City: ${apiData.name}</h6>
+        <p>Weather: ${apiData.weather[0].description}</p>
+        <p>Temp: ${apiData.main.temp}</p>
+        <p>Temp: ${apiData.main.humidity}</p>
+        <p>Speed: ${apiData.wind.speed}</p>
+        <img src="http://openweathermap.org/img/wn/${apiData.weather[0].icon}.png" />
+        `)
     })
 }
+
+function fiveDayForecast (cityName){
+    var queryUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then (function(apiData){
+        console.log(apiData);
+        var htmlString = "";
+        for (let i=0; i<apiData.list.length; i=i+8) {
+            htmlString += `
+        <div class='card'>
+        <h6>Date: ${apiData.list[i].dt_txt}</h6>
+        <p>Weather: ${apiData.list[i].weather[0].description}</p>
+        <p>Temp: ${apiData.list[i].main.temp}</p>
+        <p>Temp: ${apiData.list[i].main.humidity}</p>
+        <p>Speed: ${apiData.list[i].wind.speed}</p>
+        <img src="http://openweathermap.org/img/wn/${apiData.list[i].weather[0].icon}.png" />
+        `
+        }
+        $("#fiveDayData").html(htmlString)
+    })
+}
+
